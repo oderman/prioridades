6280fb316300606157a6193e53f8d9dfea7766ed
Jhon Oderman Mejia <jhooderman@gmail.com>
2020-08-19 09:36:08 -0500

    Endpoint revistas leer ya funcionando. Sin reestricción de método POST o GET.

---
+++ jwt-api/api.php
@@ -0,0 +1,73 @@
+<?php 
+
+	class Api extends Rest {
+		
+		public function __construct() {
+			parent::__construct();
+			$db  = new DbConnect;
+			$this->dbConn = $db->connect();
+		}
+
+		public function generateToken() {
+			$email = $this->validateParameter('email', $this->param['email'], STRING);
+			$pass = $this->validateParameter('pass', $this->param['pass'], STRING);
+			try {
+				$stmt = $this->dbConn->prepare("SELECT * FROM usuarios WHERE usr_email = :email AND usr_clave = SHA1(:pass)");
+				$stmt->bindParam(":email", $email);
+				$stmt->bindParam(":pass", $pass);
+				$stmt->execute();
+				$user = $stmt->fetch(PDO::FETCH_ASSOC);
+				if(!is_array($user)) {
+					$this->returnResponse(INVALID_USER_PASS, "Email or Password is incorrect.");
+				}
+
+				if( $user['usr_suscripcion'] == 0 ) {
+					$this->returnResponse(USER_NOT_ACTIVE, "User is not activated. Please contact to admin.");
+				}
+
+				$paylod = [
+					'iat' => time(),
+					'iss' => 'localhost',
+					'exp' => time() + (1440*60),
+					'userId' => $user['usr_id']
+				];
+
+				$token = JWT::encode($paylod, SECRETE_KEY);
+				
+				$data = ['token' => $token];
+				$this->returnResponse(SUCCESS_RESPONSE, $data);
+			} catch (Exception $e) {
+				$this->throwError(JWT_PROCESSING_ERROR, $e->getMessage());
+			}
+		}
+
+
+		public function getMagazines(){
+			$magazineId = $this->validateParameter('magazineId', $this->param['magazineId'], STRING);
+			echo $magazineId;
+
+			try{
+				$token = $this->getBearerToken();
+				$payload = JWT::decode($token, SECRETE_KEY,['HS256']);
+			 	print_r($payload);
+			} catch(Exception $e){
+				$this->throwError(ACCESS_TOKEN_ERRORS, $e->getMessage());
+			}
+		}
+
+		protected function getDbInstance(){
+			$db  = new DbConnect;
+			return $db->connect();
+		
+		}
+
+		protected function getUserIDFromToken($token){
+			$tokenParts = explode(".", $token);  
+			$tokenHeader = base64_decode($tokenParts[0]);
+			$tokenPayload = base64_decode($tokenParts[1]);
+			$jwtHeader = json_decode($tokenHeader);
+			$jwtPayload = json_decode($tokenPayload);
+			return $jwtPayload->userId;
+		}
+}
+ ?>
---
+++ jwt-api/revistas-leer.php
@@ -0,0 +1,68 @@
+<?php 
+require_once('./DbConnect.php');
+require_once('./rest.php');
+require_once('./api.php');
+require_once('./jwt.php');
+
+class RevistaPdf extends Api{
+
+    public function getParameters(){
+       
+        $idRevista = $_GET["idRevista"];
+        $this->getMagazinePdfByID($idRevista);
+
+    }
+
+    
+    public function getMagazinePdfByID($idRevista){
+
+        try{
+            $token = $this->getBearerToken();
+            $payload = JWT::decode($token, SECRETE_KEY,['HS256']);
+            
+           $db = $this->getDbInstance();
+
+           $userId = $this-> getUserIDFromToken($token);
+
+            $consultaUsr = $db->prepare("SELECT * FROM usuarios WHERE usr_id='".$userId."'");
+            $consultaUsr->execute();
+            $num = $consultaUsr->rowCount();
+            $usr = $consultaUsr->fetch();
+
+            if($num == 0){
+                $this->throwError(USER_NOT_EXIST, 'El usuarios no existe.');
+            }
+
+            if($usr['usr_suscripcion'] == 0){
+                $this->throwError(SUSCRIPTION_DISABLED, 'El usuarios no tiene suscripción activa.');
+            }
+
+            $arreglo = array();
+
+            $consulta = $db->prepare("SELECT rev_id AS id, CONCAT('https://revistaprioridades.com/admin/revistas/', rev_archivo) AS pdf FROM revistas
+            WHERE rev_id='".$idRevista."'
+            ");
+            $consulta->execute();
+            $res = $consulta->fetch(PDO::FETCH_ASSOC);
+
+            $arreglo['Magazine'] = $res;
+         
+
+           
+
+
+            $this->returnResponse(SUCCESS_RESPONSE, $arreglo);
+
+             
+        } catch(Exception $e){
+            $this->throwError(ACCESS_TOKEN_ERRORS, $e->getMessage());
+        }
+    }
+
+}
+
+$revistas = new RevistaPdf;
+
+$revistas->getParameters();
+
+?>
