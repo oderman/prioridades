e5a90259f990840c63a65ddbcd3342279e938923
Jhon Oderman Mejia <jhooderman@gmail.com>
2020-08-19 09:42:36 -0500

    Cambio del nombre del archivo de BD

--- jwt-api/usuario-info.php
+++ jwt-api/usuario-info.php
@@ -1,68 +1,120 @@
 <?php 
-require_once('./dbconnect.php');
+
+require_once('./DbConnect.php');
+
 require_once('./rest.php');
+
 require_once('./api.php');
+
 require_once('./jwt.php');
 
-class RevistaPdf extends Api{
+
+
+class UsuarioInfo extends Api{
+
+
 
     public function getParameters(){
+
        
-        $idRevista = $_GET["idRevista"];
-        $this->getMagazinePdfByID($idRevista);
+
+        $idUsuario = $_GET["idUsuario"];
+
+        $this->getUsuarioByID($idUsuario);
+
+
 
     }
 
+
+
     
-    public function getMagazinePdfByID($idRevista){
+
+    public function getUsuarioByID($idUsuario){
+
+
 
         try{
+
             $token = $this->getBearerToken();
+
             $payload = JWT::decode($token, SECRETE_KEY,['HS256']);
+
             
+
            $db = $this->getDbInstance();
+
+
 
            $userId = $this-> getUserIDFromToken($token);
 
+
+
             $consultaUsr = $db->prepare("SELECT * FROM usuarios WHERE usr_id='".$userId."'");
+
             $consultaUsr->execute();
+
             $num = $consultaUsr->rowCount();
+
             $usr = $consultaUsr->fetch();
 
+
+
             if($num == 0){
+
                 $this->throwError(USER_NOT_EXIST, 'El usuarios no existe.');
+
             }
 
-            if($usr['usr_suscripcion'] == 0){
-                $this->throwError(SUSCRIPTION_DISABLED, 'El usuarios no tiene suscripción activa.');
-            }
+
+
 
             $arreglo = array();
 
-            $consulta = $db->prepare("SELECT rev_id AS id, CONCAT('https://revistaprioridades.com/admin/revistas/', rev_archivo) AS pdf FROM revistas
-            WHERE rev_id='".$idRevista."'
+
+
+            $consulta = $db->prepare("SELECT usr_email AS email, usr_apellidos AS apellidos, usr_nombres AS nombres FROM usuarios
+
+            WHERE rev_id='".$userId."'
+
             ");
+
             $consulta->execute();
+
             $res = $consulta->fetch(PDO::FETCH_ASSOC);
 
-            $arreglo['Magazine'] = $res;
+
+
+            $arreglo['Info'] = $res;
+
          
-
-           
-
 
             $this->returnResponse(SUCCESS_RESPONSE, $arreglo);
 
+
+
              
+
         } catch(Exception $e){
+
             $this->throwError(ACCESS_TOKEN_ERRORS, $e->getMessage());
+
         }
+
     }
+
+
 
 }
 
-$revistas = new RevistaPdf;
+
+
+$revistas = new UsuarioInfo;
+
+
 
 $revistas->getParameters();
+
+
 
 ?>
