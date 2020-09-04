552b33ab824198b78ff062dcbc871876b562ea71
Jhon Oderman Mejia <jhooderman@gmail.com>
2020-09-04 13:09:51 -0500

    QUe no imprima lo del correo.

--- jwt-api/register-user.php
+++ jwt-api/register-user.php
@@ -133,10 +133,10 @@ class RegisterUser extends Api{
 
 		// Instantiation and passing `true` enables exceptions
 			$mail = new PHPMailer(true);
-			echo '<div style="display:none;">';
+			
 				try {
 					//Server settings
-					$mail->SMTPDebug = 2;                                       // Enable verbose debug output
+					$mail->SMTPDebug = 0;                                       // Enable verbose debug output
 					$mail->isSMTP();                                            // Set mailer to use SMTP
 					$mail->Host       = 'a2plcpnl0884.prod.iad2.secureserver.net';  // Specify main and backup SMTP servers
 					$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
@@ -162,7 +162,7 @@ class RegisterUser extends Api{
                     $this->throwError(NOT_SEND_EMAIL, $mail->ErrorInfo);
                     //echo "Error: {$mail->ErrorInfo}";
                 }
-			echo '</div>';
+			
 
     }
 
